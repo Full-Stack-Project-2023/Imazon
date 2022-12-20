654381280570a1e566853c2b613bf0c0ebdfad13
Kevin <kevin31yu@gmail.com>
2022-12-20 14:07:56 -0800

    improved sign in js

--- js/Sign_In.js
+++ js/Sign_In.js
@@ -2,8 +2,23 @@ $(document).ready(function(){
     $(".name").click(function() {
         window.location.href = "/index.html";
     });
+    
     $("#sign_in_form").submit(function() {
-        //require JSON to read data
-        alert("Where is home page?");
+        const email = $("#email").val();
+        const password = $("#password").val();
+        const found = checkInfo(email, password); 
+
+        if (found) {
+            //???
+            window.location.href = "/index.html";
+        }
+        else {
+            alert("Incorrect Info");
+        }
     });
 });
+
+function checkInfo(email, password) {
+    //MySQL
+    return false;
+}
