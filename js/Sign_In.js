c1b43295b37221f604c6cdda48efddee228b1d19
Kevin <kevin31yu@gmail.com>
2022-12-21 08:58:25 -0800

    sign in error

--- js/Sign_In.js
+++ js/Sign_In.js
@@ -2,23 +2,27 @@ $(document).ready(function(){
     $(".name").click(function() {
         window.location.href = "/index.html";
     });
-    
-    $("#sign_in_form").submit(function() {
-        const email = $("#email").val();
-        const password = $("#password").val();
-        const found = checkInfo(email, password); 
 
-        if (found) {
-            //???
-            window.location.href = "/index.html";
-        }
-        else {
-            alert("Incorrect Info");
-        }
+    const $sign_in_form = $("#sign_in_form");
+    $sign_in_form.submit(function(e) {
+        e.preventDefault();
+        const email = $sign_in_form.find('input[name=email]').val();
+        const password = $sign_in_form.find('input[name=password]').val();
+        //error
+        $.ajax({
+            url: '/Sign_In.html',
+            method: 'POST',
+            contentType: "text/json; charset=utf-8",
+            data: JSON.stringify({
+                email: email,
+                password: password
+            })
+        }).then(() => {
+            alert('登录成功')
+            location.assign('/index.html')
+        }, () => {
+            alert(`Email: ${email}\nPassword: ${password}`);
+            alert('失败')
+        })
     });
 });
-
-function checkInfo(email, password) {
-    //MySQL
-    return false;
-}
